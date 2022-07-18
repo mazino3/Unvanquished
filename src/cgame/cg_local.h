2403b4765dc710f6f6b417f9fd9cd7e9a2acdd92
Antoine Fontaine <antoine.fontaine@epfl.ch>
2022-07-18 23:42:43 +0200

    TMP: Broken: Juggernaut: add a second sound on death

--- src/cgame/cg_local.h
+++ src/cgame/cg_local.h
@@ -1508,6 +1508,7 @@ struct cgMedia_t
 
 	sfxHandle_t newJuggernautSound;
 	sfxHandle_t deathSound;
+	sfxHandle_t juggDeathSound;
 
 	sfxHandle_t grenadeBounceSound0;
 	sfxHandle_t grenadeBounceSound1;
